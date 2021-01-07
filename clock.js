f383a0775e30f81fc8024b5b0ae7f2af7a3ea526
WildanHawari <64912105+WildanHawari@users.noreply.github.com>
2021-01-08 00:12:45 +0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -10,10 +10,10 @@
         document.getElementById("clock").innerHTML = clock;
         
         if(jam == 0 && menit == 0 && detik == 0) {
-            let syg = 'Hai Eja, Aku sayang kamu :3';
+            let syg = 'Hai Elsa, Semoga cepet dapet doi';
             document.getElementById("note").innerHTML = syg;
-        } else if(jam == 0 && menit == 0 && detik == 11) {
-            let tdr = 'Sekarang kamu tidur yak, udah malem. Good Night :3';
+        } else if(jam == 0 && menit == 0 && detik == 12) {
+            let tdr = 'Mending sekarang tidur, dari pada halu mulu wkwkwk';
             document.getElementById("note").innerHTML = tdr;
         }
 	}
